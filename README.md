b536c280fd57c278c58709024cd94028fefa7d89
LDongGyu <ehdrbchd@naver.com>
2019-01-13 21:31:01 +0900

    예제 추가

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # kotlin_example1
 코틀린 앱 예제
 
-> 
+> 핵심 문법과 예제로 배우는 코틀린 출처  
+
+원본 : https://github.com/kukuru/roadbook
---
+++ WeatherCast/app/src/main/java/com/kotlin/weathercast/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.kotlin.weathercast
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.Loader
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.kotlin.weathercase.R
+import com.kotlin.weathercast.data.CityData
+import com.kotlin.weathercast.data.WeatherForecast
+import com.kotlin.weathercast.db.DBHanlderAnko
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),LoaderManager.LoaderCallbacks<ArrayList<WeatherForecast>> {
+
+    val LOADER_ID = 101010
+    var adapter: WeatherListViewAdapter? = null
+    val mCityArray  = ArrayList<CityData>()
+    val mDb = DBHanlderAnko(this)
+
+    companion object{
+        val SELECTED_CITY = 1100
+        val REQUEST_CITY = 1101
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mCityArray.addAll(mDb.getCityDataAll())
+        supportLoaderManager.initLoader(LOADER_ID, null, this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.add_city->{
+                val intent = Intent(this, SelectCityActivity::class.java)
+                startActivityForResult(intent, REQUEST_CITY)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onLoadFinished(loader: Loader<ArrayList<WeatherForecast>>?, data: ArrayList<WeatherForecast>) {
+        if(adapter == null){
+            adapter = WeatherListViewAdapter(this, data)
+            adapter?.setDeleteClickListener(object:View.OnClickListener{
+                override fun onClick(view:View?){
+                    val db = DBHanlderAnko(this@MainActivity)
+                    val id:String = view?.tag as String
+                    db.deleteCity(id)
+                    adapter?.removeData(id)
+                }
+            })
+            weatherList.adapter = adapter
+            weatherList.layoutManager = LinearLayoutManager(this)
+        }
+        else{
+            adapter?.updateData(data)
+        }
+        
+        progressbar.visibility = View.GONE
+    }
+
+    override fun onLoaderReset(loader: Loader<ArrayList<WeatherForecast>>?) {
+
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<ArrayList<WeatherForecast>> {
+        val loader: ForecastDataLoader = ForecastDataLoader(this, mCityArray)
+        loader.forceLoad()
+        progressbar.visibility = View.VISIBLE
+        return loader
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(resultCode)
+        {
+            SELECTED_CITY ->{
+                mCityArray.clear()
+                mCityArray.addAll(mDb.getCityDataAll())
+                supportLoaderManager.restartLoader(LOADER_ID, null, this)
+            }
+        }
+    }
+}
